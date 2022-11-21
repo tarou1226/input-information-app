9b2532e2a39a4486765aa7666af189e331c9b643
taro <teroro1226@outlook.jp>
2022-11-22 01:01:38 +0900

    create struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::io::{ Write, stdin, stdout};
+use std::io::{ Write, stdin, stdout };
+
+struct Information {
+    name: String,
+    age: i32,
+    hobby: String
+}
+
+impl Information {
+    pub fn new() -> Self {
+        Information {
+            name: "".to_string(),
+            age: 0,
+            hobby: "".to_string()
+        }
+    }
+
+    pub fn input_name(&mut self, input: &str) {
+        self.name = input.to_string();
+    }
+
+    pub fn input_age(&mut self, input: &i32) -> &str {
+        self.age = *input;
+        match input {
+            0..=5 => "babby",
+            6..=18 => "young",
+            19..=59 => "adult",
+            60..=120 => "granpa",
+            _ => panic!("error")
+        }
+    }
+
+    pub fn input_hobby(&mut self, input: &str) {
+        self.hobby = input.to_string();
+    }
+
+}
 
 fn main() {
     let mut buf = String::new();
-
-    loop {
-        print!("input -> ");
-        stdout().flush().unwrap();
-        let byte = stdin().read_line(&mut buf).unwrap();
-
-        if byte == 0 {
-            break;
-        }
-
-        print!("{}", buf);
-        buf.clear();
-    }
-
+    let mut num = 0;
+    let mut info = Information::new();
 }
